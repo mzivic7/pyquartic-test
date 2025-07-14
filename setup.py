e3e70bf625fe750e85c22170016a666b0ee337d9
mzivic7 <marko.b.zivic@gmail.com>
2025-07-14 23:22:42 +0200

    test8

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ from setuptools.command.build_py import build_py as _build_py
 
 class CustomBuild(_build_py):
     def run(self):
+
+        super().run()
+
         subprocess.check_call(["python", "build.py"])
 
         for so_file in glob.glob("*.so") + glob.glob("*.pyd"):
             target = os.path.join("pyquartic", os.path.basename(so_file))
             os.rename(so_file, target)
 
-        super().run()
 
 setup(
     cmdclass={"build_py": CustomBuild},
