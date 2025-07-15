3a0295714284b70bc6b1d8d29d27a6b1ff0cb3b2
mzivic7 <marko.b.zivic@gmail.com>
2025-07-15 13:52:04 +0200

    test16

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ import sys
 
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, PROJECT_ROOT)
+
+
+class bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
 
 class CustomBuildPy(_build_py):
     def run(self):
@@ -22,5 +29,8 @@ class CustomBuildPy(_build_py):
 setup(
     name="pyquartic",
     packages=["pyquartic"],
-    cmdclass={"build_py": CustomBuildPy},
+    cmdclass={
+        "build_py": CustomBuildPy,
+        "bdist_wheel": bdist_wheel,
+        },
 )
