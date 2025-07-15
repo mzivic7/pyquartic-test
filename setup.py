15646a71ad6af8df95f4229ac3158b8c5b7d4d4c
mzivic7 <marko.b.zivic@gmail.com>
2025-07-15 12:21:16 +0200

    test9

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 import glob
 import os
-import subprocess
+import sys
 
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, PROJECT_ROOT)
 
 class CustomBuild(_build_py):
     def run(self):
-
-        super().run()
-
-        subprocess.check_call(["python", "build.py"])
+        import build
+        build.build_numba_extensions()
 
         for so_file in glob.glob("*.so") + glob.glob("*.pyd"):
             target = os.path.join("pyquartic", os.path.basename(so_file))
             os.rename(so_file, target)
 
+        super().run()
 
 setup(
+    name="pyquartic",
+    packages=["pyquartic"],
     cmdclass={"build_py": CustomBuild},
 )
